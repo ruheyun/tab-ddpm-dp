2044f9b3c97f2cc19f6db21f9eb1163553dbd22b
ruheyun <1654033661@qq.com>
2026-03-16 16:22:47 +0800

    20260316g

--- CTGAN/mechanisms.py
+++ CTGAN/mechanisms.py
@@ -157,6 +157,7 @@ def print_dp_summary(
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', metavar='file', default='exp/adult/tvae/config.toml')
+    parser.add_argument('--epsilon', type=float, default=10)
 
     args, _ = parser.parse_known_args()
     raw_config = load_config(args.config)
@@ -166,7 +167,7 @@ def main():
         sample_size=raw_info['train_size'],
         batch_size=raw_config['train_params']['batch_size'],
         epochs=raw_config['train_params']['epochs'],
-        target_epsilon=raw_config['dp']['epsilon'],
+        target_epsilon=args.epsilon,
         target_delta=raw_config['dp']['delta'],
     )
 
