e7b066ac1ca773d43a09da2c293159fe9b80a5fb
ruheyun <1654033661@qq.com>
2026-03-16 21:34:10 +0800

    20260316qe

--- CTGAN/pipeline_tvae.py
+++ CTGAN/pipeline_tvae.py
@@ -30,7 +30,7 @@ def main():
     parser.add_argument('--eval', action='store_true',  default=False)
     parser.add_argument('--change_val', action='store_true',  default=False)
     parser.add_argument('--epsilon', type=float, default=-1)
-    parser.add_argument('--max_grad_norm', type=float, default=1)
+    parser.add_argument('--max_grad_norm', type=float, default=1.)
 
     args = parser.parse_args()
     raw_config = lib.load_config(args.config)
